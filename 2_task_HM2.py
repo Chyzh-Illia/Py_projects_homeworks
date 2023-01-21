f9a5831ef4f2f5fe83187e1dc38c2bb2c3120dd9
chyzh-i <chyzh-i@Ubuntu22.04.myguest.virtualbox.org>
2023-01-21 18:34:25 +0200

    task_is_fixed 2_task_HM2

--- 2_task_HM2.py
+++ 2_task_HM2.py
@@ -1,5 +1,5 @@
-casino_blacklist = ["John Dow", "Terry Michel", "Kevin Hart"]
-poker_blacklist = ["Jack Stanly", "Thomas Shelby", "John Dow"]
-alcohol_blacklist = ["Jacob Snow", "John Dow", "Donald Tramp"]
-black_list = set(casino_blacklist) & set(poker_blacklist) & set(alcohol_blacklist)
+casino_blacklist = {"John Dow", "Terry Michel", "Kevin Hart"}
+poker_blacklist = {"Jack Stanly", "Thomas Shelby", "John Dow"}
+alcohol_blacklist = {"Jacob Snow", "John Dow", "Donald Tramp"}
+black_list = casino_blacklist & poker_blacklist & alcohol_blacklist
 print(black_list)
