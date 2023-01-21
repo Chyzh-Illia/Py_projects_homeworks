2402dfefd23bad5c12477b9228f11e577f58e7e5
chyzh-i <chyzh-i@Ubuntu22.04.myguest.virtualbox.org>
2023-01-21 18:38:29 +0200

    5_task_HM2_is_fixed

--- 5_task_HM2.py
+++ 5_task_HM2.py
@@ -1,8 +1,4 @@
 my_list_1 = ["John Dow", "John Dow", "Marta Dow", "Marta Dow"]
-my_list_2 = []
-for i in my_list_1:
-    if i in my_list_2:
-        continue
-    else:
-        my_list_2.append(i)
-print(my_list_2)
+my_list_1.remove("John Dow")
+my_list_1.remove("Marta Dow")
+print(my_list_1)
