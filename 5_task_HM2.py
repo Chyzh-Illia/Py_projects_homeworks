b6d2633c171b604d8a24a04a14c68f4a8d11b7b7
chyzh-i <chyzh-i@Ubuntu22.04.myguest.virtualbox.org>
2023-01-23 12:03:20 +0200

    fixed_5_task_HM2

--- 5_task_HM2.py
+++ 5_task_HM2.py
@@ -1,4 +1,3 @@
-my_list_1 = ["John Dow", "John Dow", "Marta Dow", "Marta Dow"]
-my_list_1.remove("John Dow")
-my_list_1.remove("Marta Dow")
-print(my_list_1)
+my_list = ["John Dow", "John Dow", "Marta Dow", "Marta Dow"]
+list = list(dict.fromkeys(my_list))
+print(list)
