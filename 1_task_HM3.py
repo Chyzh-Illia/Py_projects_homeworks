913e2ab7d764a063fb053b18bbc1adce2a4bf1c2
chyzh-i <chyzh-i@Ubuntu22.04.myguest.virtualbox.org>
2023-01-25 20:43:14 +0200

    fixed_1_task_HM3

--- 1_task_HM3.py
+++ 1_task_HM3.py
@@ -1,9 +1,10 @@
 my_list = [1, 2, 3, 4, 5, 6, 7, 8]
 my_list_2 = []
 my_list_3 = []
-for element in range(len(my_list)):
-    if my_list[element] % 2 == 0:
-        my_list_3.append((element, my_list[element]))
+for index, value in enumerate(my_list):
+    my_tuple = index, value
+    if value % 2 == 0:
+        my_list_3.append(tuple(my_tuple))
     else:
-        my_list_2.append((element, my_list[element]))
+        my_list_2.append(tuple(my_tuple))
 print(my_list_2, my_list_3)
